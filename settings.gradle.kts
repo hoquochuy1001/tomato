844922c3fd51ce5b0719ea1164305c7616687875
hoquochuy1001 <huy.hoquoc@di4l.vn>
2023-09-11 21:55:39 +0700

    Commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "Tomato"
 include(":app")
- 
+include(":core-network")
+include(":core-localstorage")
+include(":core-data")
+include(":core-common")
+include(":core-theme")
+include(":core-designSystem")
+include(":core-model")
+include(":core-navigation")
